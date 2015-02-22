ae08ca61f978f6235fa74274f111a3886cc98000
Aaron Graham <aarograh@umich.edu>
2015-02-22 18:49:04 -0500

    Add some skeleton code for hw2 p4

--- hw2/p4/p4.cpp
+++ hw2/p4/p4.cpp
@@ -3,3 +3,43 @@
 // ASSIGNMENT : Homework 2
 // PROBLEM    : Problem 4
 // DATE       : February 23, 2015
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(void)
+{
+
+  const int N=1e6;
+  int n, i, C;
+
+  cout << "Input exponent n: " << endl;
+  cin >> n;
+
+  switch(n)
+  {
+    case 0:
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    default:
+      cout << "Error! Invalid exponent " << n << " passed to program!" << endl;
+      return -1;
+  }
+
+  for (i = 0; i < N; i++)
+  {
+    
+  }
+
+  return 0;
+
+}
