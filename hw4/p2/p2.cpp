e0f54fb8f024d61f62d9a6cae3a8829bb3c480a0
Aaron Graham <aarograh@umich.edu>
2015-04-08 16:31:52 -0400

    Corrected the calculation of sigma.  Was erroneously using probabilities to scale the two linear PDFs instead of just adding them.  Also added calculation of new omega, E, and mean energy loss statistics.

--- hw4/p2/p2.cpp
+++ hw4/p2/p2.cpp
@@ -5,6 +5,7 @@
 // DATE       : April 20, 2015
 
 #define B2 0.079407543572005
+#define PI 3.14159265358979323
 
 #include<iostream>
 #include<iomanip>
@@ -21,10 +22,10 @@ double drand(void)
 } // End drand()
 
 // Calculates the Klein-Nishina cross-section
-double sigma_KN(double C, double mu)
+double sigma_KN(double x, double mu)
 {
-  double invC = 1.0/C;
-  return B2*invC*invC*(C + invC - (1.0 - mu*mu));
+  double invx = 1.0/x;
+  return B2*invx*invx*(x + invx - (1.0 - mu*mu));
 } // End sigma_KN
 
 // Main program
@@ -34,10 +35,13 @@ int main(void)
   srand(time(NULL));
 
   // Declare variables
-  const int nSamples = 1e6;
-  const double mec2 = 0.511;
-  double E, alpha, C, mean, meansq, variance, uncertainty, test;
-  double mu, sigma, AL, AR, pL, pR;
+  int nSamples = 0;
+  const int nAccepted = 1e6;
+  const double mec2 = 0.511, oldOmega[3] = {0, 0, 1};
+  double E, alpha, x, mean, meansq, variance, uncertainty, test;
+  double gamma, mu, sigma, AL, AR, pL, pR, newE, deltaE;
+  double efficiency, meanELoss = 0.0, meanELossSq = 0.0;
+  double omega[3], Evariance, Euncertainty;
 
   // Get user options
   cout << "Neutron energy in MeV: E=";
@@ -50,13 +54,15 @@ int main(void)
   AR = sigma_KN(1.0, 1.0);
   pL = AL/(AL + AR);
   pR = 1.0 - pL;
+cout << AL << " " << AR << " " << pL << " " << pR << endl;
 
-  for (int i = 0; i < nSamples; i++)
+  for (int i = 0; i < nAccepted; i++)
   {
     sigma = 5.0;
     test = 0.0;
     while (sigma > test)
     {
+      nSamples++;
       // Get mu,sigma from trapezoid
       if (drand() < pL)
       {
@@ -66,25 +72,50 @@ int main(void)
       {
         mu = 2.0*sqrt(drand()) - 1.0;
       }
-      sigma = drand()*(pL*AL*(1.0-mu) + pR*AR*(mu+1.0));
+      sigma = drand()*(AL*(1.0-mu) + AR*(mu+1.0))/2.0;
       // evaluate KN distribution at mu
-      C = 1.0 + alpha*(1.0 - mu);
-      test = sigma_KN(C, mu);
+      x = 1.0 + alpha*(1.0 - mu);
+      test = sigma_KN(x, mu);
     }
     mean += mu;
     meansq += mu*mu;
+    gamma = 2.0*PI*drand();
+    omega[0] = mu*oldOmega[0] + (oldOmega[0]*oldOmega[2]*cos(gamma) -
+      oldOmega[1]*sin(gamma))*sqrt((1.0 - mu*mu)/(1.0 - 
+      oldOmega[2]*oldOmega[2]));
+    omega[1] = mu*oldOmega[1] + (oldOmega[1]*oldOmega[2]*cos(gamma) +
+      oldOmega[0]*sin(gamma))*sqrt((1.0 - mu*mu)/(1.0 - 
+      oldOmega[2]*oldOmega[2]));
+    omega[2] = mu*oldOmega[2] + cos(gamma)*sqrt((1.0 - mu*mu)*
+      (1.0 - oldOmega[2]*oldOmega[2]));
+//    newE = HC/E + (1.0 - mu)*H/mec2;
+ //   newE = HC/newE;
+    newE = mec2*E/(mec2 + (1.0 - mu)*E);
+    deltaE = (E - newE)/E;
+    meanELoss += deltaE;
+    meanELossSq += deltaE*deltaE;
   }
 
   // Calculate statistical quantities
-  mean /= nSamples;
-  meansq /= nSamples;
+  mean /= nAccepted;
+  meansq /= nAccepted;
   variance = meansq - mean*mean;
-  uncertainty = sqrt(variance)/(mean*sqrt(static_cast<double>(nSamples)));
+  uncertainty = sqrt(variance)/(mean*sqrt(static_cast<double>(nAccepted)));
+  efficiency = static_cast<double>(nAccepted)/static_cast<double>(nSamples);
+  meanELoss /= nAccepted;
+  meanELossSq /= nAccepted;
+  Evariance = meanELossSq - meanELoss*meanELoss;
+  Euncertainty = sqrt(Evariance)/(
+    meanELoss*sqrt(static_cast<double>(nAccepted)));
 
   // Program output
-  //TODO: output efficiency, mean relative energy loss (with given omega)
   cout << "Mean        = " << setprecision(10) << mean << endl;
   cout << "Uncertainty = " << setprecision(10) << uncertainty << endl;
+  cout << "Efficiency  = " << setprecision(10) << efficiency << endl;
+  cout << "Mean Energy Loss = " << setprecision(10) << 
+    meanELoss << endl;
+  cout << "Energy Loss Uncertainty = " << setprecision(10) << 
+    Euncertainty << endl;
 
   return 0;
 } // End main program
